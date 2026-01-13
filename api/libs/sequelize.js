8f86c742eb4969eb4f5c36f132278f95fca3eac1
Heider Arellano Oviedo <90737937+HeyderAndres@users.noreply.github.com>
2026-01-13 09:18:56 -0500

    [Feat] change database engine

--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -5,12 +5,14 @@ import { setupModels } from "../db/models/index.js";
 const USER = encodeURIComponent(config.db.user);
 const PASSWORD = encodeURIComponent(config.db.password);
 
-const URI = `postgresql://${USER}:${PASSWORD}@${config.db.host}:${config.db.port}/${config.db.database}`;
+// const URI = `postgresql://${USER}:${PASSWORD}@${config.db.host}:${config.db.port}/${config.db.database}`;
+const URI = `mysql://${USER}:${PASSWORD}@${config.db.host}:${config.db.port}/${config.db.database}`;
+
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'postgres',
+  dialect: 'mysql',
+  // dialect: 'postgres',
   logging: true,
-
 });
 
 setupModels(sequelize);
